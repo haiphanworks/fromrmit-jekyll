7a56447ed338134382d24699efa018178808c3a4
Hải Phan <hai@haiphan.design>
2019-09-28 04:37:50 +1000

    Form and Featured

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+window.onload = function () {
+    loadImageFile();
+}
+
+
 var cards = $(".directory-item").not('.is-skeleton');
 for (var i = 0; i < cards.length; i++) {
     var target = Math.floor(Math.random() * cards.length - 1) + 1;
@@ -24,3 +29,34 @@ $('#nominate .directory-filter input[type=checkbox]').on('change', function (e) 
         $(this).prop('checked', false);
     }
 });
+
+
+oFReader = new FileReader(), rFilter =
+    /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+
+oFReader.onload = function (oFREvent) {
+    var img = new Image();
+    img.onload = function () {
+        document.getElementById("originalImg").src = img.src;
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        canvas.width = 500;
+        canvas.height = 500;
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+        document.getElementById("uploadPreview").src = canvas.toDataURL();
+        document.getElementById("uploadPreview").parentElement.classList.remove('is-hidden');
+    }
+    img.src = oFREvent.target.result;
+};
+
+function loadImageFile() {
+    if (document.getElementById("uploadImage").files.length === 0) {
+        return;
+    }
+    var oFile = document.getElementById("uploadImage").files[0];
+    if (!rFilter.test(oFile.type)) {
+        alert("You must select a valid image file.");
+        return;
+    }
+    oFReader.readAsDataURL(oFile);
+}
