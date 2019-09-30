60341c93c7ac05bb7d5dafe1488fd3e8dbee324d
Hải Phan <hai@haiphan.design>
2019-09-30 10:55:14 +1000

    Form Upgrade

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,8 +49,11 @@ $('#nominate .directory-filter input[type=checkbox]').on('change', function (e) 
 
 
 // CANVAS IMAGE UPLOAD
-oFReader = new FileReader(), rFilter =
-    /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+
+
+
+var oFReader = new FileReader();
+var rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
 
 oFReader.onload = function (oFREvent) {
     var img = new Image();
@@ -60,8 +63,39 @@ oFReader.onload = function (oFREvent) {
         var ctx = canvas.getContext("2d");
         canvas.width = 500;
         canvas.height = 500;
-        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-        document.getElementById("uploadPreview").src = canvas.toDataURL();
+        ctx.fillStyle = '#fff';
+
+        var imageCoverSize = getCoverSize(
+            img.naturalWidth,
+            img.naturalHeight,
+            canvas.width,
+            canvas.height,
+            0.5,
+            0.5
+        );
+
+        ctx.drawImage(
+            img,
+            imageCoverSize.offsetLeft,
+            imageCoverSize.offsetTop,
+            imageCoverSize.width,
+            imageCoverSize.height
+        );
+
+        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imgData.data;
+        for (var i = 0; i < data.length; i += 4) {
+            if (data[i + 3] < 255) {
+                data[i] = 255;
+                data[i + 1] = 255;
+                data[i + 2] = 255;
+                data[i + 3] = 255;
+            }
+        }
+        ctx.putImageData(imgData, 0, 0);
+
+        document.getElementById("uploadPreview").src = canvas.toDataURL("image/jpeg");
+        document.getElementById("imageBase64").value = canvas.toDataURL("image/jpeg");
         document.getElementById("uploadPreview").parentElement.classList.remove('is-hidden');
     }
     img.src = oFREvent.target.result;
